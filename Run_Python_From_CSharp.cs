dec401487a916ee525438e28d1843bc8d687dc16
yeicobF <A291423@alumnos.uaslp.mx>
2021-01-12 23:17:48 -0600

    ARCHIVOS INICIALES

---
+++ Run_Python_From_CSharp.cs
@@ -0,0 +1,21 @@
+using System;
+
+/**
+ * * PROGRAMA PARA CORRER PYTHON DESDE C#.
+ * 
+ * Esto será útil para poder realizar el proyecto de la compra de sneakers
+ *  con un bot automáticamente en línea.
+ * 
+ * * Martes, 12 de enero del 2020.
+*/
+
+namespace Run_Python_From_CSharp
+{
+    class RunPythonFromCSharp{
+        // BUGFIX
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nHello, world!\n");
+        }
+    }
+}
