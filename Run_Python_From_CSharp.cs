173e12b84efe1573d4ad830b89fce56321a77d5a
yeicobF <A291423@alumnos.uaslp.mx>
2021-01-13 04:44:34 -0600

    TERMINÉ PROGRAMA. Python CORRE DESDE C#.
    - Ya terminé el programa. Ahora Python corre desde C#.
    
    - Aún tengo que experimentar más formas de hacerlo para ver qué
      resultado se obtiene dependiendo de las variables y sus valores.
    
    - FALTA: Mejorar el código e incluso separarlo con una clase que
      gestione todo lo que tenga que ver con correr desde C#. Además, poner
      métodos para cada paso.

--- Run_Python_From_CSharp.cs
+++ Run_Python_From_CSharp.cs
@@ -30,9 +30,10 @@ namespace Run_Python_From_CSharp
             // Inicializar lista de string.
             List<string> args_list = new List<string>();
             AskUserForArguments(ref args_list);
-            PrintUserInputArguments(args_list);
+            // PrintUserInputArguments(args_list);
 
-            // ExecutePythonFromCSharp(args_list.ToArray());
+            ExecutePythonFromCSharp(args_list.ToArray());
+
             Console.ReadKey(); // Esperar a presionar tecla para terminar.
         }
 
@@ -47,6 +48,7 @@ namespace Run_Python_From_CSharp
            
            // Indicar la ruta del ejecutable de Python.
            string pythonPath = @"C:\Users\games_000.ASHJAC\AppData\Local\Programs\Python\Python38-32\python.exe";
+           // Agregamos la ruta de Python al proceso que correremos.
            pythonProgram.FileName = pythonPath;
 
            // Mandar el código y los argumentos.
@@ -54,6 +56,87 @@ namespace Run_Python_From_CSharp
             // List<string> args_list = new List<string>();
             // AskUserForArguments(ref args_list);
             // PrintUserInputArguments(args_list);
+            
+            // Mandamos el nombre del código de Python, el cual está en el mismo
+            //  directorio.
+            string pythonScriptName = "python_script/receive_and_print_arguments.py";
+            /**
+             * * $ Antes de una cadena es parecido a string.Format(), pero en
+             *  lugar de escribir:
+             * 
+             *      string.Format("{0},{1},{2}", anInt, aBool, aString);
+             *  
+             *  se escribiría:
+             *      
+             *      $"{anInt},{aBool},{aString}";
+             * 
+             * ! FUENTE: What does $ mean before a string?
+             * LINK: https://stackoverflow.com/questions/31014869/what-does-mean-before-a-string 
+            */
+            
+            // OPCIÓN 1 ARGUMENTOS: Agregamos los argumentos a una lista de argumentos.
+            // pythonProgram.ArgumentList.Add(pythonScriptName);
+            // foreach(string arg in arguments){
+            //     pythonProgram.ArgumentList.Add(arg);
+            // }
+
+            // OPCIÓN 2 ARGUMENTOS: OTRA ALTERNATIVA (COMO LA QUE SALE EN EL VIDEO).
+            pythonProgram.Arguments = $"\"{pythonScriptName}\"";
+            foreach (string arg in arguments)
+            {
+                /** La cadena realmente iría como: ' "argumento"'
+                 *  Lleva un espacio porque es como si en la terminal
+                 *   corriera: 
+                 *  
+                 *       python pythonScriptName arg1 arg2 arg3 ...
+                 */
+                pythonProgram.Arguments += $" \"{arg}\"";
+            }
+
+            // 3) PROCESS CONFIGURATION
+            /** No ejecutar en la terminal. Se hace directamente desde el ejecutable.
+             *  true si queremos que se abra la terminal. 
+             * 
+             *  ! Si es true, no se pueden obtener los valores de entrada / salida.*/
+            pythonProgram.UseShellExecute = false;
+            // pythonProgram.UseShellExecute = true;
+            // Indicar que no queremos crearlo en una nueva ventana.
+            pythonProgram.CreateNoWindow = true;
+            // // Indicar que sí queremos obtener el valor que regrese el programa.
+            // pythonProgram.RedirectStandardOutput = true;
+            // // Si pasa algún error en el scrip, lo recibiremos.
+            // pythonProgram.RedirectStandardError = true;
+            // Indicar que sí queremos obtener el valor que regrese el programa.
+            pythonProgram.RedirectStandardOutput = true;
+            // Si pasa algún error en el scrip, lo recibiremos.
+            pythonProgram.RedirectStandardError = true;
+
+            // 4) EXECUTE THE PROCESS AND GET OUTPUT.
+            string errors = "";
+            // En resultados se guarda TODO lo que se imprime en el programa.
+            string results = "";
+
+            // Iniciamos el proceso y recibimos los errores y valores de regreso.
+            using(Process pythonProgramRunning = Process.Start(pythonProgram)){
+                // Obtener errores y resultados.
+                errors = pythonProgramRunning.StandardError.ReadToEnd();
+                results = pythonProgramRunning.StandardOutput.ReadToEnd();
+            }
+
+//             /** PARA CREAR TERMINAL.*/
+//             // Iniciamos el proceso y recibimos los errores y valores de regreso.
+//             // using(Process pythonProgramRunning = Process.Start(pythonProgram)){
+//             //     // Obtener errores y resultados.
+//             //     errors = pythonProgramRunning.StandardError.ReadToEnd();
+//             //     results = pythonProgramRunning.StandardOutput.ReadToEnd();
+//             // }
+// 
+//             Process.Start(pythonProgram);
+
+            // 5) DISPLAY OUTPUT
+            Console.WriteLine($"\n - ERRORES: {errors}");
+            Console.WriteLine("\n - RESULTADOS:\n");
+            Console.WriteLine(results);
        }
 
 /* ----------------- MÉTODO PARA PEDIR ARGUMENTOS AL USUARIO ---------------- */
